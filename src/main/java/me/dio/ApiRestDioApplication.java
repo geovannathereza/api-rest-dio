d859cdc911c8984eff7dd3ad262973cff561615e
Geovanna Silva <geovannatt2004@gmail.com>
2025-03-31 17:08:28 -0300

    Bugfix CORS

--- src/main/java/me/dio/ApiRestDioApplication.java
+++ src/main/java/me/dio/ApiRestDioApplication.java
@@ -2,12 +2,11 @@ package me.dio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
-@EntityScan(basePackages = "me.dio.domain.model")
 public class ApiRestDioApplication {
 
 	public static void main(String[] args) {
